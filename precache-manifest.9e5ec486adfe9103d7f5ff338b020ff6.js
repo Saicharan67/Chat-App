85b604d2bd93270fd239f587f386b7b855a0f3b6
saicharan67 <mahankalisaicharan@gmail.com>
2020-12-20 17:15:20 +0530

    Updates

---
+++ precache-manifest.9e5ec486adfe9103d7f5ff338b020ff6.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661eaec7b24d2ea337d3a83d2f307b4e",
+    "url": "/Chat-App/index.html"
+  },
+  {
+    "revision": "1612d44e225a94d33fcc",
+    "url": "/Chat-App/static/css/2.0c4bd45e.chunk.css"
+  },
+  {
+    "revision": "bce4cffef5bbb5b2f970",
+    "url": "/Chat-App/static/css/main.76f6a978.chunk.css"
+  },
+  {
+    "revision": "1612d44e225a94d33fcc",
+    "url": "/Chat-App/static/js/2.ae42e768.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/Chat-App/static/js/2.ae42e768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce4cffef5bbb5b2f970",
+    "url": "/Chat-App/static/js/main.5ddf9dd3.chunk.js"
+  },
+  {
+    "revision": "40429377859f991c1c78",
+    "url": "/Chat-App/static/js/runtime-main.efb630e4.js"
+  },
+  {
+    "revision": "b72f9c9374550a27ffad352f41633eb1",
+    "url": "/Chat-App/static/media/StartChat2.b72f9c93.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Chat-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Chat-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Chat-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Chat-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Chat-App/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "303e4a676b033a04f22e2725f3624a66",
+    "url": "/Chat-App/static/media/login.303e4a67.svg"
+  },
+  {
+    "revision": "a32792fdc9ae8c588e18fbf75c33cef7",
+    "url": "/Chat-App/static/media/sign.a32792fd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6988631a0a42d7d944805ed5a22bf6ba.js"
+  "/Chat-App/precache-manifest.9e5ec486adfe9103d7f5ff338b020ff6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Chat-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
